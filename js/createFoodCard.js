8fa52ac441e8b54da5bdaada07586e9fe23b9d6e
patriciaSR <patricia.suarez.psr@gmail.com>
2019-12-05 12:40:21 +0100

    config es-lint

--- js/createFoodCard.js
+++ js/createFoodCard.js
@@ -9,6 +9,44 @@ function createFoodCard(foodObj) {
   } = foodObj;
 
   const newCardContainer = document.createElement('li');
+  newCardContainer.classList.add('food__card');
+  newCardContainer.id = id;
+
+  const newFoodImage = document.createElement('img');
+  newFoodImage.classList.add('food__image');
+  newFoodImage.src = image;
+  newFoodImage.alt = name;
+
+  
+  const newFoodTitle = document.createElement('h3');
+  newFoodTitle.classList.add('food__title');
+  const newFoodTitleText = document.createTextNode(name);
+  newFoodTitle.appendChild(newFoodTitleText);
+
+  const newFoodDescription = document.createElement('p');
+  newFoodDescription.classList.add('food__description');
+  const newFoodDescriptionText = document.createTextNode(description);
+  newFoodDescription.appendChild(newFoodDescriptionText);
+
+  const newIngredientsList = document.createElement('ul');
+  newIngredientsList.classList.add('food__ingredients-list');
+  
+  ingredients.forEach(ingredient => {
+    const newIngredient = document.createElement('li');
+    newIngredient.classList.add('ingredients-list__item');
+    const newIngredientText = document.createTextNode(ingredient);
+    newIngredient.appendChild(newIngredientText);
+
+    newIngredientsList.appendChild(newIngredient);
+
+  })
+
+  newCardContainer.appendChild(newFoodImage);
+  newCardContainer.appendChild(newFoodTitle);
+  newCardContainer.appendChild(newFoodDescription);
+  newCardContainer.appendChild(newIngredientsList);
+
+
 
   return newCardContainer;
 
--- tests/createFoodCard.test.js
+++ tests/createFoodCard.test.js
@@ -1,12 +1,28 @@
+/* eslint-disable no-undef */
 import { mockFoodData } from './fixtures/mockFoodData.js';
 import { createFoodCard } from '../js/createFoodCard.js';
 
 describe('test createFoodCard meth', () => {
-  test('it returns a new li with food object info', () => {
+  test('it returns a new li with correct food object info', () => {
     const foodObject = mockFoodData[0];
-    
+
     const result = createFoodCard(foodObject);
 
+    const newfoodImage = result.querySelector('img');
+    const newfoodTitle = result.querySelector('h3');
+    const newFoodDescription = result.querySelector('.food__description');
+    const newFoodIngredientsList = result.querySelector('.food__ingredients-list');
+
+
     expect(result.tagName).toBe('LI');
+    expect(result.id).toBe(foodObject.id);
+
+    expect(newfoodImage.src).toBe(foodObject.image);
+    expect(newfoodImage.alt).toBe(foodObject.name);
+
+    expect(newfoodTitle.textContent).toBe(foodObject.name);
+    expect(newFoodDescription.textContent).toBe(foodObject.description);
+
+    expect(newFoodIngredientsList.tagName).toBe('UL');
   });
 });
