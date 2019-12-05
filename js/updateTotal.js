0d4de27dc14d6e87c5836e0c2c52a9024d3c13cd
George K <kasjmorales@gmail.com>
2019-12-05 18:28:10 +0100

    merge master

---
+++ js/updateTotal.js
@@ -0,0 +1,17 @@
+
+function updateTotal(price, total, action){
+    const IVA = 0.21;
+    if(typeof price === 'number') {
+    if(action === 'sum') {
+        total += price;
+    } else {
+        total -= price;
+    }
+    total *= IVA;
+    return Math.abs(total);
+    } else {
+        throw new Error('Not a valid format')
+    };
+};
+
+export { updateTotal };
