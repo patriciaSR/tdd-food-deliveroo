3208673a08a089c9482c364867f474fa970149a8
patriciaSR <patricia.suarez.psr@gmail.com>
2019-12-13 12:24:01 +0100

    add addEventListener tests

--- tests/printFoodInCart.test.js
+++ tests/printFoodInCart.test.js
@@ -1,20 +1,31 @@
 import { printFoodInCart } from '../js/printFoodInCart.js';
+import { mockFoodData } from './fixtures/mockFoodData.js';
 
 import { listElements } from './fixtures/mockVariables';
 
+import * as updateCounterModule from '../js/updateCounterFood.js';
+import * as updateTotalPriceModule from '../js/updateTotalPrice.js';
+
 describe('printFoodInCart method', () => {
   beforeEach(() => {
+    jest.mock('../js/updateCounterFood.js');
+    jest.mock('../js/updateTotalPrice.js');
+
     document.body.innerHTML = `
     <span></span>
     <ol class="shopping-cart__list">${listElements}</ol>
   `;
   });
 
+  afterEach(() => {
+    jest.resetModules();
+  });
+
   const mockedListElement = document.createElement('li');
   const mockedBtnElement = document.createElement('button');
   mockedListElement.appendChild(mockedBtnElement);
 
-  test('it add a <li> to <ol> cart list', () => {
+  test('it adds a <li> to <ol> cart list', () => {
     printFoodInCart(mockedListElement);
     const cartList = document.querySelector('ol');
     const cartListItem = document.querySelector('li');
@@ -23,7 +34,7 @@ describe('printFoodInCart method', () => {
     expect(cartListItem.tagName).toBe('LI');
   });
 
-  test('it add 2 btns and 1 counter to new <li> in <ol> cart list', () => {
+  test('it adds 2 btns and 1 counter to new <li> in <ol> cart list', () => {
     printFoodInCart(mockedListElement);
     const addBtn = document.querySelector('.food__add-btn');
     const removeBtn = document.querySelector('.food__remove-btn');
@@ -32,5 +43,27 @@ describe('printFoodInCart method', () => {
     expect(addBtn.textContent).toBe('+');
     expect(removeBtn.textContent).toBe('-');
     expect(counter.textContent).toBe('2');
+  });
+
+  test('it adds 2 addEventListener to addBtn and removeBtn in cart elements', () => {
+    const spyUpdateCounter = jest.spyOn(updateCounterModule, 'updateCounterFood');
+    spyUpdateCounter.mockImplementation(() => {});
+
+    const spyUpdateTotalPrice = jest.spyOn(updateTotalPriceModule, 'updateTotalPrice');
+    spyUpdateTotalPrice.mockImplementation(() => {});
+
+    printFoodInCart(mockedListElement, mockFoodData[0]);
+    const addBtn = document.querySelector('.food__add-btn');
+    const removeBtn = document.querySelector('.food__remove-btn');
+
+    addBtn.click();
+
+    expect(spyUpdateCounter).toHaveBeenCalled();
+    expect(spyUpdateTotalPrice).toHaveBeenCalled();
+
+    removeBtn.click();
+
+    expect(spyUpdateCounter).toHaveBeenCalled();
+    expect(spyUpdateTotalPrice).toHaveBeenCalled();
   });
 });
